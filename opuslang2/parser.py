08cbd439c0275ae6fbd5fda382823a149b1ec3b8
Brychlikov <brychlikow@gmail.com>
2020-09-22 17:22:18 +0200

    Propagate positions in parser

--- opuslang2/parser.py
+++ opuslang2/parser.py
@@ -6,4 +6,4 @@ _current_path = Path(__file__).resolve()
 _grammar_fname = _current_path.parent.joinpath("opuslang2.lark")
 
 # parser = Lark.open(_grammar_fname, parser='lalr', debug=True)
-parser = Lark.open(_grammar_fname, parser='earley')
+parser = Lark.open(_grammar_fname, parser='earley', propagate_positions=True)
